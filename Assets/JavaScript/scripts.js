fb70a48067b0465afb166b593ba4317feaf62c0d
Anthony <ao14581p@pace.edu>
2022-12-07 17:17:01 -0500

    Finished all HTML and Desktop CSS

--- Assets/JavaScript/scripts.js
+++ Assets/JavaScript/scripts.js
@@ -1,5 +1,62 @@
 function darkMode() {
-   
-    document.querySelector("body").style.backgroundColor = "black"
-   
+    document.body.classList.toggle("darkmode")
+}
+
+
+const cntFrm = document.fanSubscription
+const errs = document.querySelector("#errs")
+
+cntFrm.elements.submit.addEventListener("click", contactValidation)
+
+function contactValidation(event) {
+    let errors = []
+    for(let i=0; i<cntFrm.elements.length; i++) {
+        let field = cntFrm.elements[i]
+
+        if(field.required && field.value == "") {
+            if(field.labels) {
+                errors.push(`Please enter a value for your ${field.labels[0].innerHTML}`)
+            }
+            else {
+                errors.push("You are missing some information.")
+            }
+        }
+
+        if(field.type == "email" && !field.value.match(/^\S+@\S+\.\S+$/)) {
+            errors.push("Please enter a valid email address")
+        }
+    }
+
+    if(errors.length > 0) {
+        errs.innerHTML = errors.join("<br>")
+        errs.classList.remove("hidden")
+        event.preventDefault()
+    }
+}
+
+const cntFrm2 = document.siteFeedback
+const errss = document.querySelector("#errss")
+
+cntFrm2.elements.submit.addEventListener("click", contactValidation2)
+
+function contactValidation2(event) {
+    let errors = []
+    for(let i=0; i<cntFrm2.elements.length; i++) {
+        let field = cntFrm2.elements[i]
+
+        if(field.required && field.value == "") {
+          
+                errors.push("You must enter information into the form.")
+            }
+        }
+
+    
+    
+
+    if(errors.length > 0) {
+        errss.innerHTML = errors.join("<br>")
+        errss.classList.remove("hidden")
+        event.preventDefault()
+    }
+
 }
